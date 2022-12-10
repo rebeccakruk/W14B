b4f74d0b6521bb94681baa7f125c873d44f8ddf0
rk <rebeccakruk@outlook.com>
2022-12-09 17:50:01 -0700

    created login page, not tested

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,10 +1,22 @@
 <template>
     <div>
-        <h2>Log in</h2>
+        <h2>User Login</h2>
+        <div class="container">
+            <label for="uname">Username </label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label for="psw">Password </label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit">Login</button>
+
+        </div>
     </div>
 </template>
 
 <script>
+// import cookies from 'vue-cookies';
+// import AxiosComp from '@/components/AxiosComp.vue'
 export default {
     name: "LoginPage"
 }
